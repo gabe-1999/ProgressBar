ab56ece919d15c2c4d3093dac846beb96c9a7202
gabe-1999 <jensgabrieltrabert56@gmail.com>
2022-11-25 01:36:54 -0600

    Make progress bar smaller

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import math
 
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
-    bar = "\u2588" * int(percent) + "-" * (100 - int(percent))
+    total / 50 
+    bar = "\u2588" * int((percent / 100) * 50) + "-" * (50 - int((percent / 100) * 50))
     print(f"\r|{bar}| {percent:.2f}", end="\r")
     if progress == total:
         print()
@@ -13,4 +14,4 @@ result = []
 progress_bar(0, len(numbers))
 for i, x in enumerate(numbers):
     progress_bar(i + 1, len(numbers))
-    math.factorial(x)
+    result.append(math.factorial(x))
