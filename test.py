287938cd4a4d7e8993bd3d467fdb9269e6343aa0
gabe-1999 <jensgabrieltrabert56@gmail.com>
2022-11-25 00:18:00 -0600

    Print new line upon completion

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     bar = "\u2588" * int(percent) + "-" * (100 - int(percent))
     print(f"\r|{bar}| {percent:.2f}", end="\r")
+    if progress == total:
+        print()
 
 numbers = [x * 5 for x in range(3000, 5000)]
 result = []
@@ -12,5 +14,3 @@ progress_bar(0, len(numbers))
 for i, x in enumerate(numbers):
     progress_bar(i + 1, len(numbers))
     math.factorial(x)
-    if i == len(numbers):
-        print()
